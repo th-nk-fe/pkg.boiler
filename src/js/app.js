14c6ff4beaded885ec04ed758932286e1d9127e1
sionnnn <sionevans@gmail.com>
2015-11-10 11:35:55 +0000

    test hook

--- src/js/app.js
+++ src/js/app.js
@@ -12,7 +12,7 @@ import {Modal} from '../components/pkg.modal/modal';
  */
 import './thirdparty/slick.js';
 
-console.log('test');
+console.log('123');
 
 let worker = new Worker('/assets/js/webworkers/worker.js');
 worker.onmessage = function(e) {
